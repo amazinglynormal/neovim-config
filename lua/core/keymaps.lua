b060ff1fdff3f296daf7b481997c5a701b1d6f8d
Stephen <stephenbrady365@gmail.com>
2025-08-17 20:39:15 +0100

    update keybindings

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -34,8 +34,12 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
+-- Move selected lines up or down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move lines down in visual selection" })
-vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv", { desc = "Move lines up in visual selection" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move lines up in visual selection" })
+
+vim.keymap.set("n", "yc", "yygccp", { remap = true, desc = "Duplicate line and comment out the original line" })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
